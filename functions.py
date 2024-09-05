525ecdd4fc39a9956b69112bf3bb8e138a467043
venus-j <chumbavenus24@gmail.com>
2024-09-05 11:32:57 +0300

    done

---
+++ functions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def greet_programmer():
+    print("Hello, programmer!")
+    return('Hello, programmer!')
+
+def greet(name):
+    print(f'Hello, {name}!')
+    return(f'Hello, {name}!')
+
+def greet_with_default(name="programmer"):
+    print(f'Hello, {name}!')
+    return(f'Hello, {name}!')
+
+def add(num1, num2):
+    print (num1+num2)
+    return num1+num2
+
+def halve(number):
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
